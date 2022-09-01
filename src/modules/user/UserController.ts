d50d9eb8a438cbb19faabd15d86d9d7451d9650d
Gabriel Barreto <gabriel.devloper@gmail.com>
2022-09-01 10:42:57 -0300

    feat: finalizado a config do docker.

--- src/modules/user/UserController.ts
+++ src/modules/user/UserController.ts
@@ -6,57 +6,56 @@ import { UsersListService } from "./services/UsersListService";
 import { UserUpdateService } from "./services/UserUpdateService";
 
 class UserController {
-    async index(req: Request, res: Response) {
-        const userPrismaRepository = new UserPrismaRepository();
-        const usersListService = new UsersListService(userPrismaRepository);
+  async index(req: Request, res: Response) {
+    const userPrismaRepository = new UserPrismaRepository();
+    const usersListService = new UsersListService(userPrismaRepository);
 
-        const result = await usersListService.execute();
+    const result = await usersListService.execute();
 
-        return res.json(result);
-    }
+    return res.json(result);
+  }
 
-    async store(req: Request, res: Response) {
-        const { username, password } = req.body;
+  async store(req: Request, res: Response) {
+    const { username, password } = req.body;
 
-        const userPrismaRepository = new UserPrismaRepository();
-        const userCreateService = new UserCreateService(userPrismaRepository);
+    const userPrismaRepository = new UserPrismaRepository();
+    const userCreateService = new UserCreateService(userPrismaRepository);
 
-        const result = await userCreateService.execute({
-            username,
-            password
-        });
+    const result = await userCreateService.execute({
+      username,
+      password,
+    });
 
-        return res.status(201).json(result);
-    }
+    return res.status(201).json(result);
+  }
 
-    async update(req: Request, res: Response) {
-        const { username, password } = req.body;
-        const { id } = req.params;
+  async update(req: Request, res: Response) {
+    const { username, password } = req.body;
+    const { id } = req.params;
 
-        const data = {
-            username,
-            password
-        }
+    const data = {
+      username,
+      password,
+    };
 
-        const userPrismaRepository = new UserPrismaRepository();
-        const userUpdateService = new UserUpdateService(userPrismaRepository);
+    const userPrismaRepository = new UserPrismaRepository();
+    const userUpdateService = new UserUpdateService(userPrismaRepository);
 
-        const userUpdated = await userUpdateService.execute(data, Number(id));
+    const userUpdated = await userUpdateService.execute(data, Number(id));
 
-        return res.json(userUpdated);
-    }
+    return res.json(userUpdated);
+  }
 
-    async delete(req: Request, res: Response) {
-        const { id } = req.params;
+  async delete(req: Request, res: Response) {
+    const { id } = req.params;
 
-        const userPrismaRepository = new UserPrismaRepository();
-        const userDeleteService = new UserDeleteService(userPrismaRepository);
+    const userPrismaRepository = new UserPrismaRepository();
+    const userDeleteService = new UserDeleteService(userPrismaRepository);
 
-        const result = await userDeleteService.execute(Number(id));
+    const result = await userDeleteService.execute(Number(id));
 
-        return res.json(result);
-    }
-
+    return res.json(result);
+  }
 }
 
 export default new UserController();
